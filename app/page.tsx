4366fff740a05792cfe1e578a1701ef0c35410e4
Aizen1d <ddreataza@gmail.com>
2024-08-23 01:39:03 +0800

    responsiveness fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Hero from "@/components/home/Hero";
 
 export default function Home() {
   return (
-    <main className="px-5 lg:px-0">
+    <main className="flex justify-center items-center px-10 lg:px-0 min-h-[calc(100vh-8rem)]">
       <Hero/>
     </main>
   );
