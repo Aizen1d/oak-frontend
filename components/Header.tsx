22f24164ae75004ea94ae87b1ea54d142a31fb69
Aizen1d <ddreataza@gmail.com>
2024-08-23 01:36:37 +0800

    Change theme, adjustments to style

--- components/Header.tsx
+++ components/Header.tsx
@@ -34,8 +34,7 @@ const Header = () => {
           href={link.href}
           onClick={() => { screen === 'mobile' ? setIsHamburgerOpen(false) : null }}
           className={`${path === link.href ? 'font-bold' : 'font-normal'}
-                      ${screen === 'mobile' ? 'text-WHITE_LABEL_TEXT' : screen === 'large' ? 'text-BLACK_LABEL_TEXT' : ''}
-                      text-md`
+                      text-BLACK_LABEL_TEXT text-md`
                     }
         >
           {link.title}
@@ -46,7 +45,7 @@ const Header = () => {
 
   return (
     <>
-    <header className="sticky top-0 flex justify-between items-center px-10 h-20 bg-HEADER border-b border-slate-300 rounded-b-lg">
+    <header className="sticky top-0 z-30 flex justify-between items-center px-10 h-20 bg-HEADER border-b border-slate-300 rounded-b-lg">
       <div className="flex justify-center items-center space-x-5">
         <Image
           src="/images/Logo.png"
@@ -73,18 +72,29 @@ const Header = () => {
           width={35}
           height={35}
           onClick={() => setIsHamburgerOpen(true)}
+          className="hover:cursor-pointer"
         />
       </nav>
     </header>
 
     {/* Mobile screen: Hamburger opened */}
-      <nav className={`fixed top-0 left-0 z-30 bg-HAMBURGER_CONTENT_BG w-full
+      <nav className={`fixed top-0 left-0 z-30 bg-HEADER w-full
                     ${isHamburgerOpen ? 'h-full' : 'h-0 overflow-hidden'} transition-[height] duration-200 ease-in`}
       >
         <div className="flex justify-between items-center">
           {/* Left side */}
-          <div className="flex flex-col justify-center items-start w-1/2 ml-10 mt-24">
-            <ul className="flex flex-col space-y-5">
+          
+          <div className="flex flex-col justify-center items-start w-1/2 ml-10 mt-8">
+            <div className={`absolute top-6 left-10`}>
+              <Image
+                src="/images/Logo.png"
+                alt="Logo"
+                width={100}
+                height={100}
+                className="object-contain"
+              />
+            </div>
+            <ul className="flex flex-col space-y-5 mt-16">
               {mapNavLinks('mobile')}
             </ul>
           </div>
@@ -92,9 +102,9 @@ const Header = () => {
           {/* Right side */}
           <div className="w-1/2 flex justify-end items-end">
             <div 
-              className={`absolute top-10 right-10`}
+              className={`absolute top-7 right-10`}
               onClick={() => setIsHamburgerOpen(false)}>
-              <label className="text-white text-xl font-bold border rounded-lg px-3 py-1 hover:cursor-pointer">X</label>
+              <label className="text-BLACK_LABEL_TEXT text-xl font-bold border border-BLACK_LABEL_TEXT rounded-lg px-3 py-1 hover:cursor-pointer">X</label>
             </div>
           </div>
         </div>
