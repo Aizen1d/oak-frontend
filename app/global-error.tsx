45ecec78d3ac178bb77c6b95f01a2fd42fc3be66
Aizen1d <ddreataza@gmail.com>
2024-08-25 10:24:40 +0800

    global error page

---
+++ app/global-error.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+export default function GlobalError() {
+  return (
+    <div className="flex justify-center items-center w-full">
+      <h1 className="text-black text-3xl font-bold text-center">Something went wrong, reload the page.</h1>
+    </div>
+  )
+}
